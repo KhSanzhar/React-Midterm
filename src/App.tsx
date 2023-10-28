2a08e1e659a0c18add2ad14e2067bc654bcb0287
KhSanzhar <sanzhkhalel@gmail.com>
2023-10-28 15:54:33 +0600

    last

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import React from "react"
 import { Navigation } from "./components/Navigation"
 import { Route, Routes } from "react-router-dom"
 import { Homepage } from "./pages/homepage"
-import { PostsPage } from "./pages/postspage"
 import { ProfilePage } from "./pages/profilepage"
+import { CreatePost } from "./components/CreatePost"
 
 function App() {
   return(
@@ -11,7 +11,7 @@ function App() {
       <Navigation />
         <Routes>
           <Route path="/" element={ <Homepage /> } />
-          <Route path="/posts" element={ <PostsPage /> } />
+          <Route path="/posts" element={ <CreatePost /> } />
           <Route path="/profile" element={ <ProfilePage />} />
         </Routes>
     </>
--- src/components/CreatePost.tsx
+++ src/components/CreatePost.tsx
@@ -1,46 +1,48 @@
 import React, { useState } from 'react';
 
-const ImageForm = () => {
-    const [description, setDescription] = useState('');
-    const [image, setImage] = useState(null);
-    const [preview, setPreview] = useState(null);
 
-    const handleChange = (e: any) => {
-        if (e.target.name === 'image') {
-            setPreview(URL.createObjectURL(e.target.files[0]));
-        }
-        setImage(e.target.files[0]);
-        setDescription(e.target.value);
+
+export function CreatePost() {
+    const ImageForm = () => {
+        const [description, setDescription] = useState('');
+        const [image, setImage] = useState(null);
+        const [preview, setPreview] = useState(null);
+
+        const handleChange = (e: any) => {
+            if (e.target.name === 'image') {
+                setPreview(URL.createObjectURL(e.target.files[0]));
+            }
+            setImage(e.target.files[0]);
+            setDescription(e.target.value);
+        };
+
+        const handleSubmit = (e: any) => {
+            e.preventDefault();
+
+            setImage(null);
+            setPreview(null);
+            setDescription('');
+        };
+
+        return (
+            <div className="image-form">
+                <form onSubmit={handleSubmit}>
+                    <input 
+                        type="text"
+                        className="border py-2 px-4 mb-2 w-full outline-0"
+                        placeholder="Enter description"
+                        value={description}
+                        onChange={(event) => setDescription(event.target.value)}
+                    />
+                    <input 
+                        type="file"
+                        name="image"
+                        onChange={handleChange}
+                    />
+                    {preview && <img src={preview} alt="preview" />}
+                    <button type="submit">Add image</button>
+                </form>
+            </div>
+        );
     };
-
-    const handleSubmit = (e: any) => {
-        e.preventDefault();
-
-        setImage(null);
-        setPreview(null);
-        setDescription('');
-    };
-
-    return (
-        <div className="image-form">
-            <form onSubmit={handleSubmit}>
-                <input 
-                    type="text"
-                    className="border py-2 px-4 mb-2 w-full outline-0"
-                    placeholder="Enter description"
-                    value={description}
-                    onChange={(event) => setDescription(event.target.value)}
-                />
-                <input 
-                    type="file"
-                    name="image"
-                    onChange={handleChange}
-                />
-                {preview && <img src={preview} alt="preview" />}
-                <button type="submit">Add image</button>
-            </form>
-        </div>
-    );
-};
-
-export default ImageForm;
+}
